8a2383e59833e3cca744d554f07a15e7e8f3ad30
ATung01 <acc.tung@gmail.com>
2024-02-18 17:24:01 -0500

    first project

---
+++ first-project/project.py
@@ -0,0 +1,14 @@
+import string
+
+print(1 + 1)
+print(0.1 + 0.2)
+print(10 * 1.1 - (5.5 + 0.1))
+print("hello " + "world")
+
+color = "red"
+
+print("a " + color + " bus")
+print(string.ascii_letters)
+print(string.ascii_lowercase)
+print("primary colors: {0}, {1}, {2}".format(color, "yellow", "blue"))
+print("hello world"[::-1])
